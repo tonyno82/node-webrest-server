d95dd9fbb653fbdc1d07508124d0ae174e82d929
Antonio Rodriguez <antonio.rodriguez@rodama.com>
2023-10-06 17:56:31 +0200

    final seccion 14 (rest server)

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import path from 'path'
+import { Router } from 'express';
 
 interface Options {
     port: number;
+    routes: Router;
     public_path?: string;
 }
 
@@ -11,20 +13,29 @@ export class Server {
     private app = express()
     private readonly port: number
     private readonly publicPath: string
+    private readonly routes: Router
 
     constructor(options: Options) {
-        const {port, public_path = 'public'} = options
+        const {port, routes, public_path = 'public'} = options
         this.port = port
         this.publicPath = public_path
+        this.routes = routes
     }
 
     async start() {
 
         //* Middlewares
+        this.app.use(express.json())
+        this.app.use(express.urlencoded({extended: true}))
 
         //* Public folder
         this.app.use(express.static(this.publicPath))
 
+
+        //* Routes  
+        this.app.use(this.routes)
+
+        //* SPA
         this.app.get('*', (req, res) => {
             const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`)
             res.sendFile(indexPath)
